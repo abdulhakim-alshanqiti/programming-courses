a1b46b09f1ec5db4e2f55ba4cc5493e427c7e375
p2p-notes <abdullhakim.sami@gmail.com>
2025-10-13 01:12:26 +0300

    good state

--- 14.cpp
+++ 14.cpp
@@ -19,25 +19,34 @@
 //}
 //
 //
+//// the instructor solution
 //
-//
-//void PrintNumberPattern(int number) {
+//void PrintInvertedLetterPattern(int number) {
 //	cout << endl;
-//	for (int i = 1; i <= number; i++) {
+//	for (int i = 65 + number - 1; i >= 65; i--) {
 //
-//		for (int j = 1; j <= i; j++) {
-//			cout << i;
-//		};
-//
+//		for (int j = 1; j <= number - ((65 + number - 1) - i); j++)
+//		{
+//			cout << char(i);
+//		}
 //		cout << endl;
 //
 //	}
 //
 //}
+//// my solution
 //
+//void PrintInvertedLetterPattern(int number) {
+//	cout << endl;
+//	for (int i = number; i >= 1; i--) {
+//		for (int j = 1; j <= i; j++) {
+//			cout << char(i + 64);
+//		} cout << endl;
+//	}
+//}
 //int main()
 //{
 //	int number = ReadPositiveNumber("Please Enter a Number");
-//	PrintNumberPattern(number);
+//	PrintInvertedLetterPattern(number);
 //	return 0;
 //}
