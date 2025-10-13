e1f740e3d5d1ca5a727e975d4813ac00baa3bef3
p2p-notes <abdullhakim.sami@gmail.com>
2025-10-13 03:09:10 +0300

    good state

--- 14.cpp
+++ 14.cpp
@@ -36,14 +36,14 @@
 //}
 //// my solution
 //
-//void PrintInvertedLetterPattern(int number) {
-//	cout << endl;
-//	for (int i = number; i >= 1; i--) {
-//		for (int j = 1; j <= i; j++) {
-//			cout << char(i + 64);
-//		} cout << endl;
-//	}
-//}
+////void PrintInvertedLetterPattern(int number) {
+////	cout << endl;
+////	for (int i = number; i >= 1; i--) {
+////		for (int j = 1; j <= i; j++) {
+////			cout << char(i + 64);
+////		} cout << endl;
+////	}
+////}
 //int main()
 //{
 //	int number = ReadPositiveNumber("Please Enter a Number");
---
+++ 16.cpp
@@ -0,0 +1,50 @@
+//#include <iostream>
+//#include <cmath>
+//#include <string>
+//
+//using namespace std;
+//int ReadPositiveNumber(string message) {
+//	int number = 0;
+//	do {
+//		cout << message << endl;
+//
+//		cin >> number;
+//	} while (number <= 0);
+//
+//
+//	return number;
+//
+//}
+//
+//void PrintAllFromAAAtoZZZ()
+//{
+//	cout << "\n";
+//
+//
+//	string word = "";
+//
+//
+//	for (int i = 65; i <= 90; i++)
+//	{
+//		for (int j = 65; j <= 90; j++)
+//		{
+//			for (int k = 65; k <= 90; k++)
+//			{
+//				word.append(1, char(i));
+//				word.append(1, char(j));
+//				word.append(1, char(k));
+//				cout << word << endl;
+//
+//				word = "";
+//			}
+//		}
+//		cout << "\n____________________________\n";
+//	}
+//}
+//
+//int main()
+//{
+//
+//	PrintAllFromAAAtoZZZ();
+//	return 0;
+//}
