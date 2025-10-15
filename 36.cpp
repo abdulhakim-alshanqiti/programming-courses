a7e7d8abb5984ec6165a6e9f1d64a62366e24d4f
p2p-notes <abdullhakim.sami@gmail.com>
2025-10-15 23:12:35 +0300

    good state

---
+++ 36.cpp
@@ -0,0 +1,80 @@
+//#include <iostream>
+//#include <cstdlib>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//
+//
+//int RandomNumber(int from, int to) {
+//	return rand() % (to - from + 1) + from;
+//}
+//
+//int ReadPositiveNumber(string message) {
+//	int number = 0;
+//	do {
+//		cout << message << endl;
+//
+//		cin >> number;
+//	} while (number <= 0);
+//
+//
+//	return number;
+//
+//}
+//
+//
+//
+//
+//void PrintArray(vector<int> array, string message) {
+//
+//	if (array.size() == 0) {
+//		message += " Empty";
+//	}
+//
+//	cout << message;
+//
+//	cout << "Array Length: " << array.size() << endl;
+//
+//	for (int i = 0; i < array.size(); i++) {
+//
+//		cout << array[i] << "\t";
+//
+//
+//	}
+//	cout << endl;
+//
+//
+//
+//}
+//
+//
+//void AddArrayElement(vector<int>& array, int number) {
+//
+//	array.push_back(number);
+//}
+//vector<int> ReadArrayElements() {
+//	vector<int> array;
+//	bool AddMoreNumbers = false;
+//
+//	do {
+//
+//		AddArrayElement(array, ReadPositiveNumber("Enter A Number"));
+//		cout << "Do you want to add another number ?" << endl;
+//		cin >> AddMoreNumbers;
+//
+//	} while (AddMoreNumbers);
+//
+//	return array;
+//}
+//int main()
+//{
+//	srand((unsigned)time(NULL));
+//
+//
+//	vector<int> array = ReadArrayElements();
+//
+//	PrintArray(array, "Array 1 Elements: \n");
+//
+//
+//	return 0;
+//}
